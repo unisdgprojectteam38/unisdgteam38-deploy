31f80f23925dc7b6285857b6d5662bd8e07c3a34
weikaikhoo <weikaikhoo@gmail.com>
2024-10-09 14:33:53 +1100

    feat(header): clean comments

--- components/info/header/HeaderSection.tsx
+++ components/info/header/HeaderSection.tsx
@@ -26,6 +26,7 @@ export const HeaderSection: React.FC<HeaderSectionProps> = ({
 }) => {
   return (
     <section className={`${backgroundColor} p-8`}>
+
       {/* News Alert Bar */}
       <div className={`${newsBannerColor} flex flex-row p-4 rounded-lg mb-8`}>
         <div className="h-[87px] flex items-start">
@@ -37,6 +38,7 @@ export const HeaderSection: React.FC<HeaderSectionProps> = ({
         </div>
         
       </div>
+
       {/* Hero Section */}
       <div className="flex justify-between items-start pb-8">
         <div className="h-72 flex-col justify-center items-start gap-8 inline-flex">
@@ -48,7 +50,8 @@ export const HeaderSection: React.FC<HeaderSectionProps> = ({
           {illustrationComponent}
         </div>
       </div>
-      {/* Definition Section */}
+
+      {/* Definition Section, Subheader*/}
       <div className={"flex-col w-full justify-start items-center gap-6 inline-flex bg-white py-16 rounded-lg"}>
         <h2 className="text-center text-black text-[29px] font-medium font-['Poppins'] leading-loose">{definitionTitle}</h2>
         <p className="w-[900px] text-center text-black text-base font-normal font-['Poppins'] leading-tight">{definitionPara}</p>

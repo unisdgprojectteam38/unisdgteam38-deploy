922e9a189039b483551fec6ee618dd2d83b3b9cc
josherh <josh.yuze@student.unimelb.edu.au>
2024-10-27 11:36:36 +1100

    installed some libraries

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "SDGame",
+  description: "An innovative and engaging way to learn about the Sustainable Development Goals",
 };
 
 export default function RootLayout({

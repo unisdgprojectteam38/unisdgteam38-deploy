2bc5241d36e5f9492292cc7afe9bba7e4610211c
Shiyuan Zhou <shizhou2@student.unimelb.edu.au>
2024-10-28 20:17:54 +1100

    fix: hide api key from code

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,11 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const response = await fetch(
-    'https://newsapi.org/v2/everything?q=Sustainable+Development+Goals&sortBy=relevancy&language=en&apiKey=99d61bff343e48a6b3c2e24c4d61eb96',
+    `https://newsapi.org/v2/everything?q=Sustainable+Development+Goals&sortBy=relevancy&language=en&apiKey=${process.env.NEWS_API_KEY}`,
     {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     }
   );
